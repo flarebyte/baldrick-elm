9d09a1404e57dbc8ab21932d0c88720433ab6a43
Olivier Huin <olih@users.noreply.github.com>
2022-06-27 08:49:25 +0100

    Adds current dir

--- test/commit-message.test.ts
+++ test/commit-message.test.ts
@@ -4,7 +4,7 @@ describe('commit-message', () => {
   it('should provide a commit message', () => {
     const actual = commitMessage();
     expect(actual).toMatchInlineSnapshot(`
-      "Normalize the code structure with baldrick-elm version 0.4.0
+      "Normalize the code structure with baldrick-elm version 0.5.0
       See https://github.com/flarebyte/baldrick-elm/releases"
     `);
   });
